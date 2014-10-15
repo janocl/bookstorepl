2b28a659a221e74168116b61276a243b27072e1a
janocl <janocl84@gmail.com>
2014-10-14 21:38:06 -0300

    first commit

---
+++ MVCPL2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCPL2.Startup))]
+namespace MVCPL2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
